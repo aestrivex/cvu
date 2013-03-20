c36aa9b539da56e26b79201c7758a78b90cd6c7d
aestrivex <aestrivex@gmail.com>
2013-03-20 10:45:45 -0400

    fixed small bug in runner script

--- cvu.py
+++ cvu.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import sys 
 
-os.chdir('cvu')
-os.system('python cvu.py')
+os.chdir(os.path.join(os.path.dirname(os.path.realpath(__file__)),'cvu'))
+os.system('python cvu.py'+" "+" ".join(sys.argv[1:]))
