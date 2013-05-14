9202aa4fc2e36afb354580ed0372b04de2309415
aestrivex <aestrivex@gmail.com>
2013-05-14 15:24:22 -0400

    added license text

--- cvu/cvu_utils.py
+++ cvu/cvu_utils.py
@@ -1,4 +1,19 @@
-# (C) Roan LaPlante 2013 rlaplant@nmr.mgh.harvard.edu
+#    (C) Roan LaPlante 2013 rlaplant@nmr.mgh.harvard.edu
+#
+#	 This file is part of cvu, the Connectome Visualization Utility.
+#
+#    cvu is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from traits.api import HasTraits,Event
 
